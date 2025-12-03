38afb89950cf68971bbbde86ffbb638959da813e
mof <mof@localhost>
2025-12-03 15:59:54 +0900

    수정

--- team_c/src/main/java/org/mnu/mapper/TagMapper.java
+++ team_c/src/main/java/org/mnu/mapper/TagMapper.java
@@ -3,7 +3,7 @@ package org.mnu.mapper;
 import org.apache.ibatis.annotations.Param;
 
 public interface TagMapper {
-    // TBL_TAG에 태그 저장 (이미 존재하면 무시)
+    // TBL_TAG에 태그 저장
     public void saveTag(String tagName);
     
     // TBL_RECIPE_TAG에 관계 저장
